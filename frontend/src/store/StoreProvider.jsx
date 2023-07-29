71dbdb1b4688a5c09e6135aef1bd3aea825dba9b
Adrian <adrian.wozny@interia.pl>
2023-07-29 22:35:15 +0200

    feat. added store and request helper

---
+++ frontend/src/store/StoreProvider.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+import request from "../helpers/request";
+
+import PropTypes from 'prop-types'
+
+export const StoreContext = createContext(null);
+
+const StoreProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const [user, setUser] = useState(null);
+
+    const fetchData = async () => {
+        const { data } = await request.get('/products');
+
+        setProducts(data.products);
+    };
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    return (
+        <StoreContext.Provider value={{
+            products,
+            setProducts,
+            user,
+            setUser,
+        }}>
+            {children}
+        </StoreContext.Provider>
+    )
+}
+
+StoreProvider.propTypes = {
+    children: PropTypes.func
+}
+
+export default StoreProvider
