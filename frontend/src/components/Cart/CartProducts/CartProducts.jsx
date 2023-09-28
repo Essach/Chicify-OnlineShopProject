8f5ad81cfbc6e23d65373d4252c0df4c26a6669d
Adrian <adrian.wozny@interia.pl>
2023-09-28 17:03:13 +0200

    feat. cart mobile products hide if no products

--- frontend/src/components/Cart/CartProducts/CartProducts.jsx
+++ frontend/src/components/Cart/CartProducts/CartProducts.jsx
@@ -3,21 +3,22 @@ import './CartProducts.scss';
 
 import PropTypes from 'prop-types';
 
-const CartProducts = ({products}) => {
+const CartProducts = ({ products }) => {
+
     return (
-        <cart-products>
-            {products.map(product => <CartProduct
-                key={product.id}
-                id={product.id}
-                image={product.images[0]}
-                name={product.name}
-                reviews={product.reviews}
-                delivery={product.delivery}
-                quantity={product.currentQuantity}
-                maxQuantity={product.quantity}
-                price={product.price}
-            />)}
-        </cart-products>
+        <div className={`cart-products-${products.length > 0 ? 'shown' : 'hidden'}`}>
+                {products.map(product => <CartProduct
+                    key={product.id}
+                    id={product.id}
+                    image={product.images[0]}
+                    name={product.name}
+                    reviews={product.reviews}
+                    delivery={product.delivery}
+                    quantity={product.currentQuantity}
+                    maxQuantity={product.quantity}
+                    price={product.price}
+                />)}
+        </div>
     );
 }
 
